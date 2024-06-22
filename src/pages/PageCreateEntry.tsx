5ad80fdc457328be754f4857831c0db8d6e80553
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-22 22:21:39 +0200

    base form styled

--- src/pages/PageCreateEntry.tsx
+++ src/pages/PageCreateEntry.tsx
@@ -1,5 +1,33 @@
 export const PageCreateEntry = () => {
 	return (
-		<p>This is the info page.</p>
-	)
-}
+		<>
+			<form
+				className="createBlogEntry"
+				action="#"
+				method="post"
+				id="contact"
+			>
+				<fieldset>
+					<legend>Create New Blog Entry</legend>
+
+					<div className="rows">
+						<div className="row">
+							<label htmlFor="fullName">Name:</label>
+							<input type="text" name="fullName" id="fullName" />
+						</div>
+
+						<div className="row">
+							<label htmlFor="email">Email:</label>
+							<input type="email" name="email" id="email" />
+						</div>
+
+						<div className="row">
+							<label htmlFor="phone">Phone:</label>
+							<input type="tel" name="phone" id="phone" />
+						</div>
+					</div>
+				</fieldset>
+			</form>
+		</>
+	);
+};
