dca15e7300c5436ea6af180078ce2ed497e1d615
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-25 22:05:37 +0200

    changed title

--- src/pages/PageMakingOf.tsx
+++ src/pages/PageMakingOf.tsx
@@ -1,7 +1,7 @@
 export const PageMakingOf = () => {
 	return (
 		<section className="pageMakingOf">
-			<h2>Coding Challenge for Calimoto Interview</h2>
+			<h2>Coding Challenge for Calimoto</h2>
 			<h3>By Edward Tanguay</h3>
 		</section>
 	);
