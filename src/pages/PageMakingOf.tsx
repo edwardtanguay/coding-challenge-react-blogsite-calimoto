447e2dc030d1a0ba00fbd2d18096212e8aaed106
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-26 10:00:35 +0200

    making of page restyled

--- src/pages/PageMakingOf.tsx
+++ src/pages/PageMakingOf.tsx
@@ -2,7 +2,18 @@ export const PageMakingOf = () => {
 	return (
 		<section className="pageMakingOf">
 			<h2>Coding Challenge for Calimoto</h2>
-			<h3>By Edward Tanguay</h3>
+			<h3>By Edward Tanguay, June 2024</h3>
+			<p className="intro">
+				This application is a coding challenge that I created for the
+				company Calimoto. This page describes some page basic
+				information about how to use this site, and shows you how to
+				experience all the built-in features. For more technical
+				information, see the{" "}
+				<a href="nnn" target="_blank">
+					repository README file
+				</a>
+				.
+			</p>
 		</section>
 	);
 };
