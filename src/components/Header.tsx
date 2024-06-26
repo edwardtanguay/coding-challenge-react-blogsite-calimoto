9a1937de452f90e13f3794b88e4abe5d0d989df6
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-26 09:43:48 +0200

    site title clickable

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,11 @@ export const Header = () => {
 	return (
 		<div>
 			<div className="header">
-				<h1>My Rides</h1>
+				<h1>
+					<NavLink to="/" className="mainHeader">
+						My Rides
+					</NavLink>
+				</h1>
 				<NavLink to="/making-of" className="makingOf">
 					the making of
 				</NavLink>
